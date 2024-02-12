7b4911cab6b8f5c3018c15c5e3531b262dc42eca
Vinay <kanukuvinay75@gmail.com>
2024-02-12 19:30:05 +0530

    Major implementation is done

--- game.py
+++ game.py
@@ -1,7 +1,8 @@
 import math
 import random
-from player import HumanPlayer, ComputerPlayer
+from player import HumanPlayer1, ComputerPlayer, HumanPlayer2
 from time import sleep
+
 
 class TicTokToe:
     def __init__(self) -> None:
@@ -9,14 +10,24 @@ class TicTokToe:
         self.current_winner = None
 
     def winner(self, letter, spot):
-        row_index = spot // 3
-        row = [i for i in self.board[row_index * 3 : (row_index + 1) * 3]]
+        flag = True
+        row_index = spot // 3  # calculating the row index from the spot player  choose
+        row = [
+            i for i in self.board[row_index * 3 : (row_index + 1) * 3]
+        ]  # taking the entire row
         if all(value == letter for value in row):
             return True
-        col_index = spot % 3
-        col = [i for i in self.board[col_index:8:3]]
+        col_index = spot % 3  # calculating column index from the spot the player choose
+        col = [i for i in self.board[col_index:9:3]]
         if all(value == letter for value in col):
             return True
+        if spot % 2 == 0:
+            if all(
+                value == letter for value in self.board[0:9:4]
+            ):  # extracting a diagonal
+                return True
+            if all(value == letter for value in self.board[2:7:2]):
+                return True
 
     def print_current_board_state(self):
         current_board = [[j for j in self.board[j * 3 : (j + 1) * 3]] for j in range(3)]
@@ -32,7 +43,6 @@ class TicTokToe:
     @staticmethod
     def make_board():
         board = [" " for _ in range(9)]
-        # board = [[str(j) for j in range(j * 3, (j + 1) * 3)] for j in range(3)]
         return board
 
     @staticmethod
@@ -59,28 +69,37 @@ class TicTokToe:
 
 def play(game, x_player, y_player, play_game=True):
     print(game.print_board())
-    letter = "x"
+    letter = "X"
+    flag = False
     while game.empty_squres():
-        print(game.empty_squres())
-        if letter == "x":
+        if letter == "X":
             spot = x_player.get_move(game)
             game.make_move(spot, letter)
-            game.winner(letter=letter, spot=spot)
         else:
             spot = y_player.get_move(game)
             game.make_move(spot=spot, letter=letter)
-            print("Computer making a move")
-            sleep(2)
-            game.winner(letter=letter, spot=spot)
+            print(f"{y_player.letter} making a move")
         print(game.print_current_board_state())
         if game.winner(letter=letter, spot=spot):
             print(f"{letter} player wins")
+            flag = True
             break
-        letter = "y" if letter == "x" else "x"
+        letter = "O" if letter == "X" else "X"
+    if not flag:
+        print("It is a tie")
 
 
 if __name__ == "__main__":
-    x_player = HumanPlayer("X")
-    y_player = ComputerPlayer("Y")
+    while True:
+        try:
+            oponent = int(input("Choose your oponent \nComputer:(0) or Human:(1): "))
+            break
+        except Exception:
+            print("Enter integers only")
+    x_player = HumanPlayer1("X")
+    if oponent:
+        y_player = HumanPlayer2("O")
+    else:
+        y_player = ComputerPlayer("O")
     t = TicTokToe()
     play(t, x_player, y_player, True)
--- player.py
+++ player.py
@@ -1,31 +1,52 @@
 import random
 import math
 
+
 class Player:
     def __init__(self, letter) -> None:
         self.letter = letter
- 
+
 
 class ComputerPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
+
     def get_move(self, game):
         return random.choice(game.available_moves())
 
 
-class HumanPlayer(Player):
+class HumanPlayer1(Player):
     def __init__(self, letter):
         super().__init__(letter)
 
     def get_move(self, game):
-        avalable_moves_now  = game.available_moves()
+        avalable_moves_now = game.available_moves()
         try:
-            spot = int(input("Enter your human move :"))
-            if spot not in avalable_moves_now :
+            spot = int(input(f"Make your move player {self.letter}: "))
+            if spot not in avalable_moves_now:
                 raise ValueError
         except ValueError:
             print("Invalid move")
 
         return spot
 
-    
+
+class HumanPlayer2(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        avalable_moves_now = game.available_moves()
+        try:
+            spot = int(input(f"Make your move player {self.letter}: "))
+            if spot not in avalable_moves_now:
+                raise ValueError
+        except ValueError:
+            print("Invalid move")
+
+        return spot
+
+
+class GeniusComputerPlayer:
+    def __init__(self) -> None:
+        pass
